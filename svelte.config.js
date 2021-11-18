17de947b4d8baa570868836d6b70699b67e87aff
Dankimhaejun <dan.kimhaejun@gmail.com>
2021-11-18 13:43:53 +0900

    feat: connext netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,17 @@
 import preprocess from 'svelte-preprocess';
+import netlify from '@sveltejs/adapter-netlify'; //<--- 추가
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+        // Consult https://github.com/sveltejs/svelte-preprocess
+        // for more information about preprocessors
+        preprocess: preprocess(),
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+        kit: {
+                adapter: netlify(), //<--- 추가
+                // hydrate the <div id="svelte"> element in src/app.html
+                target: '#svelte'
+        }
 };
 
 export default config;
